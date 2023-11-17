4c898f3be53f4c47fe4d3abb4938420a929f7f2f
piyawatluk <piyawatluknatin@gmail.com>
2023-11-17 15:58:15 +0700

    image tranformation test and metrix import

--- matrix_test/image_tranformer.py
+++ matrix_test/image_tranformer.py
@@ -2,27 +2,19 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 import sys
-import logging
 from PIL import Image
-
-logging.basicConfig(filename="log.txt", filemode="w")
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
 
 
 np.set_printoptions(threshold=sys.maxsize)
 
-file = "test_pix3.jpg"
+file = r"C:\Users\piyaw\PycharmProjects\suffering\test_pix3.jpg"
+
+size_x = 960
+size_y = 960
 
 # Load the image
 image = cv2.imread(file)
-
-new_width = 45
-new_height = 45
-
-# Resize the image
-resized_image = cv2.resize(image, (new_width, new_height))
 
 # Convert the image to HSV
 hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -30,9 +22,10 @@ hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 # Extract the Value (brightness) channel from image
 value_channel = image[:, :, 2]
 
-image_array = np.array(value_channel)
-img = Image.fromarray(image_array)
-img.show()
+image_array_gray = np.array(value_channel)
+image_array_hsv = np.array(hsv_image)
+img_1 = Image.fromarray(image_array_gray)
+img_1.show()
 
 size = value_channel.shape
 
@@ -49,11 +42,16 @@ plt.subplot(1, 3, 3)
 plt.imshow(value_channel, cmap='gray')
 plt.title('Value Channel (Grayscale)')
 
+for i in range(size_x):
+    for j in range(size_y):
+        num = image_array_gray[i, j]
+        if num in range(230, 240):
+            image_array_gray[i, j] = 0
+        else:
+            image_array_gray[i, j] = 255
 
+img_2 = Image.fromarray(image_array_gray)
+img_2.show()
 
-print(image_array)
 print(size)
-
-logger.info(image_array)
-logger.info(size)
 plt.show()
