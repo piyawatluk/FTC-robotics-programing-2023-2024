44440cbf8953cf6eb566ab92b24a90842f5d0d37
winwin2671 <88149505+winwin2671@users.noreply.github.com>
2023-11-17 16:23:54 +0700

    red hsv detection and mask other colors to black

--- opencv_HSV/test.py
+++ opencv_HSV/test.py
@@ -7,18 +7,6 @@ file = "/Users/User/Documents/GitHub/FTC-robotics-programing/opencv_HSV/test_pix
 # Load the image
 image = cv2.imread(file)
 
-# # Display the the images
-# plt.figure(figsize=(10, 5))
-# plt.subplot(1, 2, 1)
-# plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-# plt.title('RGB')
-# plt.subplot(1, 2, 2)
-# plt.imshow(cv2.cvtColor(image, cv2.COLOR_RGB2HSV))
-# plt.title('HSV')
-# plt.tight_layout()
-# plt.show()
-
-
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
 # red
@@ -32,12 +20,13 @@ c, h = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 for c in c:
     area = cv2.contourArea(c)
     if area > 1000:
-        cv2.drawContours(image, c, -1, (255, 0, 0), 2)
+        cv2.drawContours(image, [c], -1, (255, 0, 0), 2)
         print(area)
 
 cv2.imshow('res', res)
 cv2.imshow('image', image)
 cv2.imshow('mask', mask)
 
-if cv2.waitKey(0) & 0xff == ord('q'):
-    cv2.destroyAllWindows()
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
