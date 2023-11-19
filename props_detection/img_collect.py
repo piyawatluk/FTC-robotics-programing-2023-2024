c11d39f2ff7d9ff5e47e12a6fac15438a5df9af3
winwin2671 <88149505+winwin2671@users.noreply.github.com>
2023-11-19 11:46:09 +0700

    update collect img function

--- props_detection/img_collect.py
+++ props_detection/img_collect.py
@@ -1,15 +1,18 @@
 import cv2
 cap = cv2.VideoCapture(1)
 path = './img/'
-r, p = (0, 0, 0, 0)
+r, b = (0, 0)
 while True:
     _, frame = cap.read()
     key = cv2.waitKey(1) & 0xFF
+
+    # press "r" to save prop_red
     if key == ord('r'):
         r += 1
         cv2.imwrite(path + 'prop_red_' + str(r) + '.png', frame)
-    if key == ord('p'):
-        p += 1
-        cv2.imwrite(path + 'prop_blue' + str(p) + '.png', frame)
 
-    cv2.imshow('frame', frame)
+    # press "b" to save prop_blue
+    if key == ord('b'):
+        b += 1
+        cv2.imwrite(path + 'prop_blue_' + str(b) + '.png', frame)
+        cv2.imshow('frame', frame)
